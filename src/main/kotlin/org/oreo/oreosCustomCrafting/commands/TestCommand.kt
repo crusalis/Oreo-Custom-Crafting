b5b1d29a9152a2f0cf34a6aefdaac493594cf830
oreotroll <oreotrollturbo@gmail.con>
2024-11-01 02:35:13 +0100

    WORKKSASDASDASDASD

--- src/main/kotlin/org/oreo/oreosCustomCrafting/commands/TestCommand.kt
+++ src/main/kotlin/org/oreo/oreosCustomCrafting/commands/TestCommand.kt
@@ -1,7 +1,6 @@
 package org.oreo.oreosCustomCrafting.commands
 
 import net.md_5.bungee.api.ChatColor
-import org.bukkit.Material
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
@@ -9,7 +8,6 @@ import org.bukkit.command.TabCompleter
 import org.bukkit.entity.Player
 import org.oreo.oreosCustomCrafting.CustomCrafting
 import org.oreo.oreosCustomCrafting.menu.CustomCraftingInventory
-import org.oreo.oreosCustomCrafting.utils.Utils
 
 
 class TestCommand(private val plugin: CustomCrafting) : CommandExecutor, TabCompleter {
@@ -20,38 +18,19 @@ class TestCommand(private val plugin: CustomCrafting) : CommandExecutor, TabComp
             return false
         }
 
-        val itemInHand = sender.inventory.itemInMainHand
+        if (!sender.isOp){
+            sender.sendMessage("${ChatColor.RED} This command can only be used by players.")
+            return true
+        }
 
         if (args.isEmpty()) {
-            sender.sendMessage("${ChatColor.RED}Please specify a subcommand: save, give <name>, or all.")
+            sender.sendMessage("${ChatColor.RED}Please specify a subcommand")
             return false
         }
 
         when (args[0].lowercase()) {
 
-            "save" -> {
-                if (itemInHand.type == Material.AIR) {
-                    sender.sendMessage("${ChatColor.RED}You must hold an item to save.")
-                    return false
-                }
-
-                Utils.saveCustomItemAsFile(itemInHand, plugin)
-                itemInHand.amount = 0
-                sender.sendMessage("${ChatColor.GREEN}Item saved.")
-            }
-
-            "give" -> {
-                if (args.size < 2) {
-                    sender.sendMessage("${ChatColor.RED}Please specify the name of the item to give.")
-                    return false
-                }
-                val itemName = args[1]
-                val customItem = Utils.getCustomItem(itemName)
-                sender.inventory.addItem(customItem)
-                sender.sendMessage("${ChatColor.GREEN}Gave you one $itemName.")
-            }
-
-            "craftin" -> {
+            "crafting" -> {
 
                 if (args.size < 2) {
                     sender.sendMessage("${ChatColor.RED}Please specify the name of the recipe.")
@@ -60,18 +39,6 @@ class TestCommand(private val plugin: CustomCrafting) : CommandExecutor, TabComp
 
                 val recipeName = args[1]
                 CustomCraftingInventory(sender, recipeName, plugin)
-            }
-
-            "all" -> {
-                for (item in CustomCrafting.customItems.values) {
-                    sender.inventory.addItem(item)
-                }
-                sender.sendMessage("${ChatColor.GREEN}All custom items have been added to your inventory.")
-            }
-
-            else -> {
-                sender.sendMessage("${ChatColor.RED} Invalid subcommand. Use save, give <name>, or all.")
-                return false
             }
         }
         return true
@@ -84,12 +51,7 @@ class TestCommand(private val plugin: CustomCrafting) : CommandExecutor, TabComp
         args: Array<out String>
     ): List<String>? {
         return when (args.size) {
-            1 -> listOf("save", "give", "all","craftin").filter { it.startsWith(args[0], ignoreCase = true) }
-            2 -> if (args[0].equals("give", ignoreCase = true)) {
-                CustomCrafting.customItems.keys.filter { it.startsWith(args[1], ignoreCase = true) }
-            } else {
-                emptyList()
-            }
+            1 -> listOf("crafting").filter { it.startsWith(args[0], ignoreCase = true) }
 
             else -> emptyList()
         }
